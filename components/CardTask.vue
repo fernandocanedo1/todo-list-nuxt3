4446dcd4475ba9efbb89b49a208c633323951b7a
fernandocanedo1 <fern.luck@gmail.com>
2023-04-26 14:17:02 -0300

    funcionalidade - Deletar Tarefa

--- components/CardTask.vue
+++ components/CardTask.vue
@@ -5,25 +5,24 @@
       <div>
         <div class="text-overline mb-1">COMPLETO</div>
         <div class="text-h6 mb-1">{{ card.name }}</div>
-        <div class="text-caption line-clamp-2">{{card.description}}</div>
+        <div class="text-caption line-clamp-2">{{ card.description }}</div>
       </div>
     </v-card-item>
     <v-card-actions class="d-flex justify-end">
       <!-- <v-btn variant="outlined"> Button </v-btn> -->
-      <v-btn text @click="deleteCard">Delete</v-btn>
+      <v-btn text @click="task.removeItem(card.name)">Delete</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 <script>
+import { useCardStore } from "/stores/card.ts";
 export default defineComponent({
   props: {
-    card: Object
+    card: Object,
   },
-  setup() {},
-  methods: {
-    deleteCard() {
-      console.log("deleted:" );
-    },
+  setup() {
+    const task = useCardStore();
+    return{task}
   },
 });
 </script>
